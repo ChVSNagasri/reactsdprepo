9042ad5a6ad509ac210e8bf056f8aa67d97114a0
ChVSNagasri <2500030484@kluniversity.in>
2026-02-24 12:40:36 +0530

    project 3

--- src/customer/CustomerLogout.jsx
+++ src/customer/CustomerLogout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function CustomerLogOut() {
+export default function CustomerLogout() {
   return (
-    <div>CustomerLogOut</div>
+    <div>CustomerLogout</div>
   )
 }
