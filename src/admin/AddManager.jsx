c2f717380f0599c1dd1c5dfc71231b71a3f704ee
ChVSNagasri <2500030484@kluniversity.in>
2026-02-24 16:18:14 +0530

    modified AddManager.jsx

--- src/admin/AddManager.jsx
+++ src/admin/AddManager.jsx
@@ -15,7 +15,6 @@ export default function AddManager() {
 
   const [successMessage, setSuccessMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
